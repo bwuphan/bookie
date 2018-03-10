1f155277428a9c90b004eb39d094e6ffbf3816c9
bobby <bwuphan@gmail.com>
2018-03-10 15:10:22 -0800

    Update userModel to normalized auth0 user profile

--- server/users/userModel.js
+++ server/users/userModel.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  username: { type: String, required: true },
-  books: [new Schema({
-    bookname: String,
-    recipeIds: [Number],
-  })],
+  name     : { type: String, required: true },
+  nickname : { type: String, required: true },
+  picture  : { type: String, required: true },
+  user_id  : { type: String, required: true },
+
 });
 
 module.exports = mongoose.model('User', UserSchema);
